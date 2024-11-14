24e9a4d4e87ef1ec663e86172fa4667b8936c719
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-11-15 01:03:19 +0530

    Lesson4

--- Lesson2/myproject/posts/models.py
+++ Lesson2/myproject/posts/models.py
@@ -8,4 +8,5 @@ class Post(models.Model):
     slug: models.SlugField() # type: ignore
     date: models.DateTimeField(auto_now_add=True) # type: ignore
 
-
+    def __str__(self):
+        return self.title
