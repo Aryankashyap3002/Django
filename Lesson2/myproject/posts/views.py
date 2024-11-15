e94a45cb3f57dc88c54bffc5bdfb3eb48a6aa8e9
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-11-15 16:19:46 +0530

    Lesson7

--- Lesson2/myproject/posts/views.py
+++ Lesson2/myproject/posts/views.py
@@ -5,4 +5,9 @@ from .models import Post
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
-    return render(request, 'posts/posts_list.html', {"posts": posts})
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def posts_page(request, slug):
+    posts = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': posts})
